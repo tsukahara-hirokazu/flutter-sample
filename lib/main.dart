468fbf49807280be47ba130562c9086a93347d8e
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-03-13 13:40:50 +0900

    Navigation

--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,45 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: "Flutter layout Demo",
-        home: Scaffold(
-          appBar: AppBar(
-            title: Text("Flutter layout demo"),
+        title: "Navigation",
+        home: FirstScreen(),
+      );
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text("1番目のルート"),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => SecondScreen(),
+                ),
+              );
+            },
+            child: Text("次の画面へ"),
           ),
-          body: Center(
-            child: Text(
-              "Hello World",
-            ),
+        ),
+      );
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: Text("2番目のルート"),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text("戻る"),
           ),
         ),
       );
