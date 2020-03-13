a7ce520149d79d76bf029440f192b8bdca7edf2e
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-03-13 11:57:06 +0900

    3.2.1.4 Flutter layout demo on Material Design

--- lib/main.dart
+++ lib/main.dart
@@ -2,43 +2,18 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) => MaterialApp(
-        title: "Flutter Demo",
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
+        title: "Flutter layout Demo",
         home: Scaffold(
-          body: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text("You have pushed the button this many times:"),
-                Text(
-                  '$_counter',
-                  style: Theme.of(context).textTheme.display1,
-                ),
-              ],
-            ),
+          appBar: AppBar(
+            title: Text("Flutter layout demo"),
           ),
-          floatingActionButton: FloatingActionButton(
-            onPressed: _incrementCounter,
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
+          body: Center(
+            child: Text(
+              "Hello World",
+            ),
           ),
         ),
       );
