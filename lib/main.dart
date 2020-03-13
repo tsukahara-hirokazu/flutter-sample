61cdf32388533eb08cec8b6880089f7ac3eb3327
htsukahara <tsukahara.hirokazu@ajax-labo.com>
2020-03-13 11:28:14 +0900

    3.1.2 Stateless Widget'
    q
    exit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        title: "Fultter Demo",
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+          body: Center(
+            child: Text(
+              "Flutter Demo Home Page",
+              style: Theme.of(context).textTheme.display1,
+            ),
+          ),
+        ),
+      );
+}
